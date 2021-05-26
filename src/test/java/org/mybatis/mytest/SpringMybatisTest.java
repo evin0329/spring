2573e84f222fc198d3085d96e547cbf5b02c395b
hepeng <2410465519@qq.com>
2021-05-27 01:02:13 +0800

    test

--- src/test/java/org/mybatis/mytest/SpringMybatisTest.java
+++ src/test/java/org/mybatis/mytest/SpringMybatisTest.java
@@ -1,14 +1,6 @@
 package org.mybatis.mytest;
 
-import org.mybatis.spring.TestMapper;
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.stereotype.Component;
 
 import java.util.concurrent.locks.LockSupport;
 
